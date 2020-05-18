fcb7e2e2be766f598f8b256f2263436a6bd770eb
Sylvain <sylvain.nadeau@nexio.com>
2020-05-18 12:41:32 -0400

    Get JUnit debug infos

--- jyatlon/src/main/java/jyatlon/core/StructBuilder.java
+++ jyatlon/src/main/java/jyatlon/core/StructBuilder.java
@@ -173,7 +173,7 @@ public class StructBuilder<T> extends YATLBaseListener {
 		Object getValue() {
 			if (children.isEmpty())
 				return ctx.getText();
-			throw new IllegalStateException();
+			throw new IllegalStateException("BUG: " + this.getArgName() + " text: " + ctx.getText() + " args: " + children.size());
 		}
 		<T> Struct<T> toStruct(Class<T> currentClass) throws Exception {
 //			Class<T> currentClass = builder.getSubclassForName(extractStructName());
